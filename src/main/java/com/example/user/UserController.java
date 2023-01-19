4cfdd65763aeb1a25bc23c1af735c66fd94adbff
kurata.ryosuke <kurata.ryosuke@technopro.com>
2023-01-19 15:56:43 +0900

    新規登録画面の表示成功

--- src/main/java/com/example/user/UserController.java
+++ src/main/java/com/example/user/UserController.java
@@ -9,13 +9,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.entity.Role;
 import com.example.entity.User;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
@@ -26,43 +27,25 @@ public class UserController {
     }
 
     /**
-     * 管理者一覧画面表示
+     * ユーザー新規登録画面表示
      *
      * @param model
-     * @return 管理者一覧画面
-     */
-    @GetMapping
-    public String listUsers(@RequestParam(required = false) String keyword, Model model) {
-        // 全管理者情報の取得
-        List<User> listUsers = userService.listAll(keyword);
-        model.addAttribute("listUsers", listUsers);
-        model.addAttribute("keyword", keyword);
-        return "users/users";
-    }
-
-    /**
-     * 管理者新規登録画面表示
-     *
-     * @param model
-     * @return 管理者新規登録画面
+     * @return ユーザー新規登録画面
      */
     @GetMapping("/new")
     public String newUser(Model model) {
         // 新規登録用に、空の管理者情報作成
         User user = new User();
-        // 全ロール情報の取得
-        List<Role> listRoles = userService.listRoles();
         model.addAttribute("user", user);
-        model.addAttribute("listRoles", listRoles);
         return "users/user_form";
     }
 
     /**
-     * 管理者登録・更新処理
+     * ユーザー登録・更新処理
      *
-     * @param user 管理者情報
+     * @param user ユーザー情報
      * @param ra
-     * @return 管理者一覧画面
+     * @return スレッド一覧画面
      */
     @PostMapping("/save")
     public String saveUser(User user, RedirectAttributes ra) {
@@ -72,17 +55,17 @@ public class UserController {
             return "redirect:/products/new";
         }
 
-        // 重複チェック
+        //ユーザー情報の重複チェック
         if (!userService.checkUnique(user)) {
             ra.addFlashAttribute("error_message", "重複しています");
             return "redirect:/products/new";
         }
 
-        // 管理者情報の登録
+        //ユーザー情報の登録
         userService.save(user);
         // 登録成功のメッセージを格納
         ra.addFlashAttribute("success_message", "登録に成功しました");
-        return "redirect:/users";
+        return "redirect:/threads";
     }
 
     /**
