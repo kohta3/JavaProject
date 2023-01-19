3d1420526f399dcdb0d769bad489e5c83cc6169c
kurata.ryosuke <kurata.ryosuke@technopro.com>
2023-01-19 17:42:47 +0900

    不要な処理の削除と登録処理の追加

--- src/main/java/com/example/user/UserController.java
+++ src/main/java/com/example/user/UserController.java
@@ -1,18 +1,13 @@
 package com.example.user;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.example.entity.Role;
 import com.example.entity.User;
 
 @Controller
@@ -34,7 +29,7 @@ public class UserController {
      */
     @GetMapping("/new")
     public String newUser(Model model) {
-        // 新規登録用に、空の管理者情報作成
+        // 新規登録用に、空のユーザー情報作成
         User user = new User();
         model.addAttribute("user", user);
         return "users/user_form";
@@ -49,70 +44,30 @@ public class UserController {
      */
     @PostMapping("/save")
     public String saveUser(User user, RedirectAttributes ra) {
-        // 入力値のチェック
+        System.out.println("ここまできたよ・・・・・・・・・・・");
+    	//入力された文字数のチェック
         if (!userService.isValid(user.getEmail(), user.getName())) {
-            ra.addFlashAttribute("error_message", "入力に誤りがあります");
-            return "redirect:/products/new";
+            ra.addFlashAttribute("error_message", "メールアドレスまたはユーザー名の文字数がオーバーしています");
+            return "redirect:/users/new";
         }
 
-        //ユーザー情報の重複チェック
-        if (!userService.checkUnique(user)) {
-            ra.addFlashAttribute("error_message", "重複しています");
-            return "redirect:/products/new";
+        //ユーザー情報のメールアドレス重複チェック
+        if (!userService.UserEmailcheckUnique(user)) {
+            ra.addFlashAttribute("error_message", "既に使用されているメールアドレスです");
+            return "redirect:/users/new";
+        }
+
+        //ユーザー情報のユーザー名重複チェック
+        if (!userService.UserNamecheckUnique(user)) {
+            ra.addFlashAttribute("error_message", "既に使用されているユーザー名です。");
+            return "redirect:/users/new";
         }
 
         //ユーザー情報の登録
         userService.save(user);
         // 登録成功のメッセージを格納
-        ra.addFlashAttribute("success_message", "登録に成功しました");
+        ra.addFlashAttribute("success_message", "ユーザーの新規登録に成功しました");
         return "redirect:/threads";
     }
-
-    /**
-     * 管理者詳細画面表示
-     *
-     * @param id 管理者ID
-     * @param model
-     * @param ra
-     * @return 管理者詳細画面
-     */
-    @GetMapping("/detail/{id}")
-    public String detailUser(@PathVariable(name = "id") Long id, Model model, RedirectAttributes ra) {
-        try {
-            // 管理者IDに紐づく管理者情報取得
-            User user = userService.get(id);
-            model.addAttribute("user", user);
-            return "users/user_detail";
-        } catch (NotFoundException e) {
-            ra.addFlashAttribute("error_message", "対象のデータが見つかりませんでした");
-            return "redirect:/users";
-        }
-    }
-
-    /**
-     * 管理者編集画面表示
-     *
-     * @param id 管理者ID
-     * @param model
-     * @param ra
-     * @return 管理者編集画面
-     */
-    @GetMapping("/edit/{id}")
-    public String editUser(@PathVariable(name = "id") Long id, Model model, RedirectAttributes ra) {
-        try {
-            // 管理者IDに紐づく管理者情報取得
-            User user = userService.get(id);
-            model.addAttribute("user", user);
-        } catch (NotFoundException e) {
-            ra.addFlashAttribute("error_message", "対象のデータが見つかりませんでした");
-            return "redirect:/users";
-        }
-
-        // 全ロール情報の取得
-        List<Role> listRoles = userService.listRoles();
-        model.addAttribute("listRoles", listRoles);
-        return "users/user_edit";
-    }
-
 
 }
--- src/main/java/com/example/user/UserService.java
+++ src/main/java/com/example/user/UserService.java
@@ -1,13 +1,10 @@
 package com.example.user;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.example.entity.Role;
 import com.example.entity.User;
 
 @Service
@@ -26,41 +23,16 @@ public class UserService {
         this.passwordEncoder = passwordEncoder;
     }
 
-    /**
-     * 管理者情報検索処理
-     *
-     * @param keyword 検索キーワード
-     * @return 管理者情報のリスト
-     */
-    public List<User> listAll(String keyword) {
-        // 検索キーワードがあった場合
-        if (keyword != null && !keyword.isEmpty()) {
-            return userRepository.search(keyword);
-        }
-        // それ以外の場合
-        else {
-            return userRepository.findAll();
-        }
-    }
 
     /**
-     * ロール情報全件取得処理
+     * IDに紐づくユーザー情報取得処理
      *
-     * @return ロール情報のリスト
-     */
-    public List<Role> listRoles() {
-        return roleRepository.findAll();
-    }
-
-    /**
-     * IDに紐づく管理者情報取得処理
-     *
-     * @param id 管理者ID
-     * @return 管理者情報
+     * @param id ユーザーID
+     * @return ユーザー情報
      * @throws NotFoundException
      */
     public User get(Long id) throws NotFoundException {
-        // IDに紐づく管理者情報が存在するかの確認
+        // IDに紐づくユーザー情報が存在するかの確認
         if (!this.exists(id)) {
             throw new NotFoundException();
         }
@@ -68,52 +40,52 @@ public class UserService {
     }
 
     /**
-     * 管理者情報登録処理
+     * ユーザー情報登録・更新処理
      *
-     * @param user 保存したい管理者情報
-     * @return 保存した管理者情報
+     * @param user 保存したいユーザー情報
+     * @return 保存したユーザー情報
      */
     public User save(User user) {
-        // 管理者情報を更新する場合
-        if (user.getId() != null) {
-            // 更新対象の管理者情報を取得
-            User existingUser = userRepository.findById(user.getId()).get();
-            // 保存したい管理者情報のパスワードが空の場合
-            if (user.getPassword().isEmpty()) {
-                // 保存したい管理者情報に以前のパスワードを格納
-                user.setPassword(existingUser.getPassword());
-            } else {
-                // パスワードのハッシュ化
-                String encodedPassword = encodePassword(user.getPassword());
-                // ハッシュ化したパスワードを格納
-                user.setPassword(encodedPassword);
-            }
-        }
-        // 管理者情報を新規登録する場合
-        else {
+//        //ユーザー情報を更新する場合
+//        if (user.getId() != null) {
+//            //更新対象のユーザー情報を取得
+//            User existingUser = userRepository.findById(user.getId()).get();
+//            //保存したいユーザー情報のパスワードが空の場合
+//            if (user.getPassword().isEmpty()) {
+//                //保存したいユーザー情報に以前のパスワードを格納
+//                user.setPassword(existingUser.getPassword());
+//            } else {
+//                //パスワードのハッシュ化
+//                String encodedPassword = encodePassword(user.getPassword());
+//                //ハッシュ化したパスワードを格納
+//                user.setPassword(encodedPassword);
+//            }
+//        }
+        //ユーザー情報を新規登録する場合
+//        else {
             // パスワードのハッシュ化
             String encodedPassword = encodePassword(user.getPassword());
             // ハッシュ化したパスワードを格納
             user.setPassword(encodedPassword);
-        }
+//        }
         return userRepository.save(user);
     }
 
     /**
-     * 管理者情報の入力値チェック
+     * ユーザー情報の入力値チェック
      *
      * @param email メールアドレス
-     * @param name 管理者名
+     * @param name ユーザー名
      * @return true:正常の入力値 false:異常な入力値
      */
     public boolean isValid(String email, String name) {
-        // メールアドレスの判定（10文字から50文字まで）
-        if (email.length() < 10 || email.length() > 50) {
+        //メールアドレスの判定（10文字から254文字まで）
+        if (email.length() < 10 || email.length() > 254) {
             return false;
         }
 
-        // 管理者名の判定（1文字から10文字まで）
-        if (name.length() < 1 || name.length() > 10) {
+        //ユーザー名の判定（1文字から100文字まで）
+        if (name.length() < 1 || name.length() > 100) {
             return false;
         }
 
@@ -121,12 +93,12 @@ public class UserService {
     }
 
     /**
-     * 管理者情報のメールアドレス重複チェック
+     * ユーザー情報のメールアドレス重複チェック
      *
-     * @param name 重複確認したい管理者情報
+     * @param name 重複確認したいメールアドレス
      * @return true:重複なし false:重複あり
      */
-    public boolean checkUnique(User user) {
+    public boolean UserEmailcheckUnique(User user) {
         boolean isCreatingNew = (user.getId() == null || user.getId() == 0);
         User userByEmail = userRepository.findByEmail(user.getEmail());
 
@@ -143,17 +115,25 @@ public class UserService {
     }
 
     /**
-     * IDに紐づくユーザー情報削除処理
+     * ユーザー情報のユーザー名重複チェック
      *
-     * @param id ユーザーID
-     * @throws NotFoundException
+     * @param name 重複確認したいユーザー名
+     * @return true:重複なし false:重複あり
      */
-    public void delete(Long id) throws NotFoundException {
-        // IDに紐づく管理者情報が存在するかの確認
-        if (!this.exists(id)) {
-            throw new NotFoundException();
+    public boolean UserNamecheckUnique(User user) {
+        boolean isCreatingNew = (user.getId() == null || user.getId() == 0);
+        User userByName = userRepository.findByEmail(user.getName());
+
+        if (isCreatingNew) {
+            if (userByName != null) {
+                return false;
+            }
+        } else {
+            if (userByName != null && userByName.getId() != user.getId()) {
+                return false;
+            }
         }
-        userRepository.deleteById(id);
+        return true;
     }
 
     /**
@@ -168,7 +148,7 @@ public class UserService {
     /**
      * ユーザー情報の存在チェック
      *
-     * @param name 確認したい管理者情報のID
+     * @param name 確認したいユーザー情報のID
      * @return true:存在する false:存在しない
      */
     private boolean exists(Long id) {
