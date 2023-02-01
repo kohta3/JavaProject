9956816a058f2944d627d00fee63adc5d616f145
unknown <Okamura.Kota@technopro.com>
2023-02-01 17:35:36 +0900

    予測変換

--- src/main/resources/static/js/base.js
+++ src/main/resources/static/js/base.js
@@ -13,6 +13,7 @@ $("#select-order").change(function(){
 	$(".selectBox-top").submit();
 });
 
+//アニメタイトル予測変換
 function searchTitle(title) {
 	let url ='https://api.annict.com/v1/works?access_token=pzj3Fri6UyklcwKv5VU4m4EVSURmFraLAlBpx0VsR0c&per_page=30&filter_title='+title;
 	$.ajax({
@@ -33,3 +34,27 @@ function searchTitle(title) {
 		console.log("予測ワードがありません");
 	});
 }
+
+var obj1 = document.getElementById("selfile");
+
+obj1.addEventListener("change", function(evt){
+  var file = evt.target.files;
+  var reader = new FileReader();
+
+  //dataURL形式でファイルを読み込む
+  reader.readAsDataURL(file[0]);
+
+  //ファイルの読込が終了した時の処理
+  reader.onload = function(){
+    var dataUrl = reader.result;
+
+    //読み込んだ画像とdataURLを書き出す
+    document.getElementById("threadPostImg").innerHTML = "<img src='" + dataUrl + "'>";
+  }
+},false);
+
+
+$("#clearImg").on('click',function(){
+	$("#threadPostImg").empty();
+	$("#selfile").val(null);
+})
